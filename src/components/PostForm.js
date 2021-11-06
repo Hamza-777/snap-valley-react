4906bdd8160cb9f0e4df3978dc022d731b73ed78
Hamza <hamzararrani77@gmail.com>
2021-11-06 19:40:55 +0530

    unique keys

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import React, { useState } from 'react';
 import './PostForm.css';
 
@@ -8,7 +9,7 @@ const PostForm = (props) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         const newPost = {
-            id: 4,
+            id: uuidv4(),
             image,
             text,
         };
